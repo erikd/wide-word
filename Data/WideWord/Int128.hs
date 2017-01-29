5a5cadbb3fdc27fac72f6e52e834b3c89f8d02bd
Erik de Castro Lopo <erikd@mega-nerd.com>
2017-01-29 17:17:14 +1100

    Int128: Remove flakey rewrite rules
    These rules were causing some operations like addition to fail in the
    test suite for really obvious cases.

--- Data/WideWord/Int128.hs
+++ Data/WideWord/Int128.hs
@@ -143,18 +143,8 @@ instance Storable Int128 where
 
 {-# RULES
 "fromIntegral :: Int128 -> Int128" fromIntegral = id :: Int128 -> Int128
-  #-}
-
-{-# RULES
-"fromIntegral :: Int -> Int128"     fromIntegral = \(I# i#) -> Int128 (W64# 0##) (W64# (int2Word# i#))
-"fromIntegral :: Word- > Int128"    fromIntegral = Int128 0 . fromIntegral
-"fromIntegral :: Word32 -> Int128"  fromIntegral = Int128 0 . fromIntegral
-"fromIntegral :: Word64 -> Int128"  fromIntegral = Int128 0
-
-"fromIntegral :: Int128 -> Int"     fromIntegral = \(Int128 _ w) -> fromIntegral w
-"fromIntegral :: Int128 -> Word"    fromIntegral = \(Int128 _ w) -> fromIntegral w
-"fromIntegral :: Int128 -> Word32"  fromIntegral = \(Int128 _ w) -> fromIntegral w
-"fromIntegral :: Int128 -> Word64"  fromIntegral = \(Int128 _ w) -> w
+"fromIntegral :: Word128 -> Int128" fromIntegral = \(Word128 a1 a0) -> Int128 a1 a0
+"fromIntegral :: Int128 -> Word128" fromIntegral = \(Int128 a1 a0) -> Word128 a1 a0
   #-}
 
 -- -----------------------------------------------------------------------------
