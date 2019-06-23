fa3891d6f6c1a0e80b7616c371bccbbbd2bc3251
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-06-23 12:00:32 +1000

    tests: Update generators to test corner cases better

--- test/Test/Data/WideWord/Gen.hs
+++ test/Test/Data/WideWord/Gen.hs
@@ -1,41 +1,17 @@
 module Test.Data.WideWord.Gen where
 
-import           Data.Int (Int8, Int16, Int32, Int64)
+import           Data.Bits (shiftL)
 import           Data.WideWord
-import           Data.Word (Word8, Word16, Word32, Word64)
+import           Data.Word (Word32, Word64)
 
 import           Hedgehog (Gen)
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 
 
-genInt8 :: Gen Int8
-genInt8 =
-  Gen.int8 Range.constantBounded
-
-genInt16 :: Gen Int16
-genInt16 =
-  Gen.int16 Range.constantBounded
-
-genInt32 :: Gen Int32
-genInt32 =
-  Gen.int32 Range.constantBounded
-
-genInt64 :: Gen Int64
-genInt64 =
-  Gen.int64 Range.constantBounded
-
 genInt128 :: Gen Int128
 genInt128 =
-  Int128 <$> genWord64 <*> genWord64
-
-genWord8 :: Gen Word8
-genWord8 =
-  Gen.word8 Range.constantBounded
-
-genWord16 :: Gen Word16
-genWord16 =
-  Gen.word16 Range.constantBounded
+  Int128 <$> genBiasedWord64 <*> genBiasedWord64
 
 genWord32 :: Gen Word32
 genWord32 =
@@ -45,6 +21,28 @@ genWord64 :: Gen Word64
 genWord64 =
   Gen.word64 Range.constantBounded
 
+-- | Generate 'Word64' in one of five categories;
+--    * the full range
+--    * small values near zero
+--    * large values near maxBound :: Word64
+--    * values near maxBound / 2 :: Word64
+--    * values near maxBound :: Word32
+genBiasedWord64 :: Gen Word64
+genBiasedWord64 =
+  Gen.choice
+    [ Gen.word64 (Range.linear 0 maxBound)
+    , Gen.word64 (Range.linear 0 100)
+    , (-) maxBound <$> Gen.word64 (Range.linear 0 100)
+    , Gen.word64 (Range.linear (halfMax - 100) (halfMax + 100))
+    , Gen.word64 (Range.linear (bits32 - 100) (bits32 + 100))
+    ]
+  where
+    bits32 :: Word64
+    bits32 = 1 `shiftL` 32
+
+    halfMax :: Word64
+    halfMax = maxBound `div` 2
+
 genWord128 :: Gen Word128
 genWord128 =
-  Word128 <$> genWord64 <*> genWord64
+  Word128 <$> genBiasedWord64 <*> genBiasedWord64
