f26f51d6308705269d876f5ac2e69353a6889871
parsonsmatt <parsonsmatt@gmail.com>
2022-11-23 14:44:56 -0700

    isolate compat

--- src/Data/WideWord/Int128.hs
+++ src/Data/WideWord/Int128.hs
@@ -60,10 +60,6 @@ import GHC.Word (Word64 (..), Word32, byteSwap64)
 import GHC.IntWord64
 #endif
 
-#if MIN_VERSION_base(4,17,0)
-import GHC.Prim (Word64#, wordToWord64#, word64ToWord#, Int64#)
-#endif
-
 import Data.Primitive.Types (Prim (..), defaultSetByteArray#, defaultSetOffAddr#)
 
 import Data.Hashable (Hashable,hashWithSalt)
@@ -74,54 +70,7 @@ import qualified GHC.Base
 import GHC.Base (int2Word#, subWordC#, plusWord2#, or#, minusWord#, timesWord2#, word2Int#, xor#, and#, not#, plusWord#, timesWord#)
 #endif
 
-#if MIN_VERSION_base(4,17,0)
-plusWord2# :: Word64# -> Word64# -> (# Word64#, Word64# #)
-plusWord2# a b =
- case GHC.Base.plusWord2# (word64ToWord# a) (word64ToWord# b) of
-   (# a', b' #) ->
-     (# wordToWord64# a', wordToWord64# b' #)
-
-timesWord2# :: Word64# -> Word64# -> (# Word64#, Word64# #)
-timesWord2# a b =
-  case GHC.Base.timesWord2# (word64ToWord# a) (word64ToWord# b) of
-    (# a', b' #) ->
-     (# wordToWord64# a', wordToWord64# b' #)
-
-int2Word# :: Int64# -> Word64#
-int2Word# = GHC.Base.int64ToWord64#
-
-minusWord# :: Word64# -> Word64# -> Word64#
-minusWord# a b =
-  wordToWord64# (GHC.Base.minusWord# (word64ToWord# a) (word64ToWord# b))
-
-subWordC# :: Word64# -> Word64# -> (# Word64#, Int64# #)
-subWordC# a b =
-  case GHC.Base.subWordC# (word64ToWord# a) (word64ToWord# b) of
-    (# a', b' #) ->
-     (# wordToWord64# a', GHC.Base.intToInt64# b' #)
-
-not# :: Word64# -> Word64#
-not# = GHC.Base.not64#
-
-or# :: Word64# -> Word64# -> Word64#
-or# = GHC.Base.or64#
-
-xor# :: Word64# -> Word64# -> Word64#
-xor# = GHC.Base.xor64#
-
-and# :: Word64# -> Word64# -> Word64#
-and# = GHC.Base.and64#
-
-timesWord# :: Word64# -> Word64# -> Word64#
-timesWord# = GHC.Base.timesWord64#
-
-plusWord# :: Word64# -> Word64# -> Word64#
-plusWord# = GHC.Base.plusWord64#
-
-word2Int# :: Word64# -> Int64#
-word2Int# = GHC.Base.word64ToInt64#
-
-#endif
+import Data.WideWord.Compat
 
 #if MIN_VERSION_base(4,17,0)
 #define ONE (wordToWord64# 1##)
