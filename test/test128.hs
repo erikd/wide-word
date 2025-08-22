0f724ac4b73cf271b7bf43ce316494ea41771319
Erik de Castro Lopo <erikd@mega-nerd.com>
2025-08-22 17:57:09 +1000

    Split the 64 and 128 bit tests into 2 separate test exes

---
+++ test/test128.hs
@@ -0,0 +1,18 @@
+import           Control.Monad (unless)
+
+import           System.Exit (exitFailure)
+
+import qualified Test.Data.WideWord.Int128
+import qualified Test.Data.WideWord.Word128
+
+main :: IO ()
+main = runTests
+  [ Test.Data.WideWord.Int128.tests
+  , Test.Data.WideWord.Word128.tests
+  ]
+
+runTests :: [IO Bool] -> IO ()
+runTests tests = do
+  result <- and <$> sequence tests
+  unless result
+    exitFailure
--- test/test64.hs
+++ test/test64.hs
@@ -2,15 +2,11 @@ import           Control.Monad (unless)
 
 import           System.Exit (exitFailure)
 
-import qualified Test.Data.WideWord.Int128
 import qualified Test.Data.WideWord.Word64
-import qualified Test.Data.WideWord.Word128
 
 main :: IO ()
 main = runTests
-  [ Test.Data.WideWord.Int128.tests
-  , Test.Data.WideWord.Word64.tests
-  , Test.Data.WideWord.Word128.tests
+  [ Test.Data.WideWord.Word64.tests
   ]
 
 runTests :: [IO Bool] -> IO ()
