ae69fc2881aa54a1c8d1c87f481c9bda0726f98e
Erik de Castro Lopo <erikd@mega-nerd.com>
2026-01-21 11:18:50 +1100

    test/laws.hs: Add test for Int256

--- test/laws.hs
+++ test/laws.hs
@@ -24,6 +24,7 @@ allPropsApplied =
   [ ("Int128", allLaws (Proxy :: Proxy Int128))
   , ("Word64", allLaws (Proxy :: Proxy Word64))
   , ("Word128", allLaws (Proxy :: Proxy Word128))
+  , ("Int256", allLaws (Proxy :: Proxy Int256))
   , ("Word256", allLaws (Proxy :: Proxy Word256))
   ]
 
@@ -87,6 +88,28 @@ instance Arbitrary Word256 where
 instance Arbitrary Int128 where
   arbitrary = Int128 <$> arbitrary <*> arbitrary
 
+instance Arbitrary Int256 where
+  arbitrary =
+    Int256
+      <$> arbitraryBoundedIntegral <*> arbitraryBoundedIntegral
+      <*> arbitraryBoundedIntegral <*> arbitraryBoundedIntegral
+  shrink x
+    | x == 0 = []
+    | x == 1 = [0]
+    | x == 2 = [0,1]
+    | x == 3 = [0,1,2]
+    | otherwise =
+        let y = x `shiftR` 1
+            z = y + 1
+            w = div (x * 9) 10
+            p = div (x * 7) 8
+         in catMaybes
+              [ if y < x then Just y else Nothing
+              , if z < x then Just z else Nothing
+              , if w < x then Just w else Nothing
+              , if p < x then Just p else Nothing
+              ]
+
 -- These are used to make sure that 'Num' behaves properly.
 instance Semiring Word128 where
   zero = 0
@@ -106,6 +129,12 @@ instance Semiring Int128 where
   plus = (+)
   times = (*)
 
+instance Semiring Int256 where
+  zero = 0
+  one  = 1
+  plus = (+)
+  times = (*)
+
 -- These are used to make sure that plus is associative
 instance Semigroup Word128 where
   (<>) = (+)
@@ -119,3 +148,5 @@ instance Semigroup Word256 where
 instance Semigroup Int128 where
   (<>) = (+)
 
+instance Semigroup Int256 where
+  (<>) = (+)
